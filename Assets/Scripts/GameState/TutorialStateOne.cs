c6d22004ddaa63009f44a420bdceea9435896f19
Newt <guoching130@gmail.com>
2021-04-03 16:57:23 +0800

    game state

---
+++ Assets/Scripts/GameState/TutorialStateOne.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialStateOne : IGameState
+{
+    private GameManager game_manager;
+
+    public TutorialStateOne(GameManager _manager)
+    {
+        this.game_manager = _manager;
+    }
+
+    public override void OnStart()
+    {
+        
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnFixedUpdate()
+    {
+
+    }
+
+    public override void OnShutDown()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameState/TutorialStateThree.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialStateThree : IGameState
+{
+    private GameManager game_manager;
+
+    public TutorialStateThree(GameManager _manager)
+    {
+        this.game_manager = _manager;
+    }
+
+    public override void OnStart()
+    {
+        
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnFixedUpdate()
+    {
+
+    }
+
+    public override void OnShutDown()
+    {
+        
+    }
+}
